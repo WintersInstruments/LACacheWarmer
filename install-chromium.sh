ba7366c59800b026b5151d9ddb07ff559f571c57
ltam <ltam@winters.com>
2025-04-25 18:59:27 -0400

    installed sh for linux

--- install-chromium.sh
+++ install-chromium.sh
@@ -4,11 +4,13 @@
 apt-get update
 apt-get install -y wget ca-certificates curl unzip
 
-# Set the version of Chromium
-CHROMIUM_VERSION="chrome-linux"
+# Set the version of Chromium (adjust for the correct version)
+CHROMIUM_VERSION="901912" # You can update this as needed to match the specific version you require
 
-# Download and extract Chromium
-wget https://storage.googleapis.com/chromium-browser-snapshots/Windows/901912/chrome-linux.zip
+# Download and extract Chromium for Linux (using the Linux version)
+wget https://storage.googleapis.com/chromium-browser-snapshots/Windows/$CHROMIUM_VERSION/chrome-linux.zip
+
+# Unzip the downloaded file into the correct directory
 unzip chrome-linux.zip -d /opt/render/project/.chromium/
 
 # Clean up the zip file
